7f57d94fa55039d39115b141cb64631708e253bf
Jeery <g3127252465@gmail.com>
2023-11-05 21:04:00 -0600

    End of section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,15 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "G");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Mustafa");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Julia");
+
+INSERT INTO Users (Id, UserName)
+VALUES (5, "Jane");
